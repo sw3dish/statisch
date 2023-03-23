17a0beaaeb7b1a355e7b1cee890155d3f9cfbb04
Colin Burr <colin@colin.id>
2023-03-22 23:13:54 -0400

    Add directory traversal

--- lib/grossglockner.ex
+++ lib/grossglockner.ex
@@ -1,6 +1,25 @@
 defmodule Grossglockner do
   def main(_argv) do
-    # Expecting content at /content
-    IO.puts(File.ls("content"))
+    gather_markdown_files("./content")
+  end
+
+  def gather_markdown_files(path) do
+    cond do
+      File.regular?(path) ->
+        case Path.extname(path) do
+          ".md" -> [path]
+          _ -> []
+        end
+      File.dir?(path) ->
+        case File.ls(path) do
+          {:ok, paths} ->
+            paths
+            |> Enum.map(&Path.join(path, &1))
+            |> Enum.flat_map(&gather_markdown_files/1)
+          {:error, reason} ->
+            IO.puts("Could not build #{path}: #{reason}")
+        end
+      true -> []
+    end
   end
 end
