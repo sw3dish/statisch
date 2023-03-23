03a9690b48970737d59769580810071812fcc224
Colin Burr <colin@colin.id>
2023-03-22 23:16:02 -0400

    Add file splitting + metadata parsing + markdown

--- lib/grossglockner.ex
+++ lib/grossglockner.ex
@@ -1,6 +1,9 @@
 defmodule Grossglockner do
   def main(_argv) do
     gather_markdown_files("./content")
+    |> Enum.map(&split_file/1)
+    |> Enum.map(&parse_metadata/1)
+    |> Enum.map(&transform_contents/1)
   end
 
   def gather_markdown_files(path) do
@@ -22,4 +25,42 @@ defmodule Grossglockner do
       true -> []
     end
   end
+
+  # returns either {:ok, path, {metadata, contents}}
+  # or {:error, path, reason}
+  def split_file(path) do
+    with {ok, contents} <- File.read(path),
+         split_contents <- String.split(contents, "---%%%---"),
+         length <- String.length(split_contents)
+    do
+      case length do
+        3 ->
+          {:ok, path, {split_contents[1], split_contents[2]}}
+        _ ->
+          {:error, path, "No metadata found!"}
+      end
+    else
+      err -> err
+    end
+  end
+
+  def parse_metadata({:ok, path, {metadata, contents}}) do
+    case JSON.decode(metadata) do
+      {:ok, json_decoded_metadata} ->
+        {:ok, path, {json_decoded_metadata, contents}}
+      {:error, reason} ->
+        {:error, path, reason}
+    end
+  end
+  def parse_metadata(error = {:error, _, _}), do: error
+
+  def transform_contents({:ok, path, {metadata, contents}}) do
+    case Earmark.as_html(contents) do
+      {:ok, html_doc, _} ->
+        {:ok, path, {metadata, html_doc}}
+      {:error, _html_doc, error_messages} ->
+        {:error, path, error_messages}
+    end
+  end
+  def transform_contents(error = {:error, _, _}), do: error
 end
