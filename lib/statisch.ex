0f61faf8a5bdeb011091ebc42cb398e3b07dd798
Colin Burr <colin@colin.id>
2023-04-03 23:03:26 -0400

    Add layout support

--- lib/statisch.ex
+++ lib/statisch.ex
@@ -4,6 +4,7 @@ defmodule Statisch do
   @content_dir "./content"
   @output_dir "./output"
   @template_dir "./templates"
+  @base_layout_path "./templates/layouts/base.html.eex"
 
   def main(_argv) do
     gather_markdown_files(@content_dir)
@@ -11,7 +12,7 @@ defmodule Statisch do
     |> Enum.map(&split_file/1)
     |> Enum.map(&parse_metadata/1)
     |> Enum.map(&transform_contents/1)
-    |> Enum.map_reduce(%{}, &inject_into_template/2)
+    |> Enum.map_reduce(initialize_template_cache(), &inject_into_template/2)
     |> drop_cache()
     |> Enum.map(&write_file/1)
     # inject into template
@@ -79,7 +80,7 @@ defmodule Statisch do
           {:ok, path, {new_metadata, contents}}
         rescue
           ArgumentError ->
-            missing_fields = Metadata.required_keys -- Map.keys(decoded_metadata)
+            missing_fields = Metadata.required_keys() -- Map.keys(decoded_metadata)
             missing_fields_string = Enum.map_join(missing_fields, ", ", &Atom.to_string/1)
             {:error, path, "Required metadata fields missing: #{missing_fields_string}"}
         end
@@ -105,13 +106,22 @@ defmodule Statisch do
 
   def inject_into_template({:ok, path, {metadata, contents}}, cache) do
     template_name = metadata.template
+
     case put_into_cache(cache, template_name) do
       {:ok, cache} ->
-        doc = EEx.eval_string(cache[template_name], assigns: [
+        assigns = %{
           body: contents,
-          title: metadata.title,
-        ])
+          title: metadata.title
+        }
+
+        # render the inner content
+        inner_content = EEx.eval_string(cache[template_name], assigns: assigns)
+        # place it into the global layout
+        assigns = Map.put(assigns, :inner_content, inner_content)
+        doc = EEx.eval_string(cache[:base], assigns: assigns)
+
         {{:ok, path, {metadata, doc}}, cache}
+
       {:error, _reason} ->
         {{:error, path, "Could not open #{template_name}.eex for reading!"}, cache}
     end
@@ -121,11 +131,22 @@ defmodule Statisch do
 
   def put_into_cache(cache, template_name) do
     template_path = Path.join(@template_dir, "#{template_name}.eex")
-    case File.read(template_path) do
-      {:ok, contents} ->
-        cache = Map.put(cache, template_name, contents)
+
+    case Map.get(cache, template_name) do
+      # if we can't find the template in the cache, add it
+      nil ->
+        case File.read(template_path) do
+          {:ok, contents} ->
+            cache = Map.put(cache, template_name, contents)
+            {:ok, cache}
+
+          error ->
+            error
+        end
+
+      # if we can, pass the cache back so we can access later
+      _ ->
         {:ok, cache}
-      error -> error
     end
   end
 
@@ -134,19 +155,29 @@ defmodule Statisch do
   end
 
   def write_file({:ok, path, {metadata, contents}}) do
-    trimmed_path = path
-                   |> String.trim_leading(@content_dir)
-                   |> String.trim_trailing(".md")
+    trimmed_path =
+      path
+      |> String.trim_leading(@content_dir)
+      |> String.trim_trailing(".md")
+
     output_path = Path.join(@output_dir, "#{trimmed_path}.html")
+
     with :ok <- File.mkdir_p(Path.dirname(output_path)),
-         :ok <- File.write(output_path, contents)
-    do
+         :ok <- File.write(output_path, contents) do
       {:ok, path, {metadata, contents}}
     else
       {:error, _reason} ->
         {:error, path, "Could not write to file #{output_path}"}
     end
-
   end
+
   def write_file(error = {:error, _, _}), do: error
+
+  def initialize_template_cache() do
+    base = File.read!(@base_layout_path)
+
+    %{
+      base: base
+    }
+  end
 end
